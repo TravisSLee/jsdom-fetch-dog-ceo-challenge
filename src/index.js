38e269e550984d7a26472402e418ed11b1be9129
TravisSLee <travisslee223@gmail.com>
2021-03-17 10:37:35 -0400

    commit tree 8a8ffa2138f62d0b47aae99a37030bb259fbe520

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
-console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
+const dogContainer = document.getElementById('dog-image-container')
+document.addEventListener("DOMContentLoaded",init);
+
+function init(){
+    fetchDog();
+}
+
 function fetchDog(){
-    
+    fetch(imgUrl, { method: 'GET'})      
+    .then(r => r.json())
+    .then(data => {
+        data.message.forEach(d =>{
+            dogContainer.src(d) 
+        })
+        })
 }
